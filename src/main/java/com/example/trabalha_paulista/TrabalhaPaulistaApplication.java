12b82922ba9b1e70eac7105a8ba93ee2bdc131ae
André Luiz <andreluizdesantanasilva123@gmail.com>
2026-05-04 14:58:40 -0300

    Add files via upload

---
+++ src/main/java/com/example/trabalha_paulista/TrabalhaPaulistaApplication.java
@@ -0,0 +1,13 @@
+package com.example.trabalha_paulista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhaPaulistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhaPaulistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trabalha_paulista/TrabalhaPaulistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabalha_paulista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhaPaulistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
